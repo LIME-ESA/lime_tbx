674c44cb47d00ad2bb7a71e2b0dca4cb1a477542
Pieter De Vis <pieter.de.vis@npl.co.uk>
2022-06-28 11:59:44 +0100

    shorten intergate_elis and add u_integrate_elis

--- lime_tbx/spectral_integration/spectral_integration.py
+++ lime_tbx/spectral_integration/spectral_integration.py
@@ -52,6 +52,7 @@ class SpectralIntegration(ISpectralIntegration):
 
     def convolve_srf(self, ch_wlens, ch_srf, wlens, elis):
         elis_ids=[wlens.index(wl) for wl in ch_wlens]
+        print(ch_srf,elis[0])
         ch_signal=[np.trapz(ch_wlens,ch_srf*subelis[elis_ids]) for subelis in elis]
 
     def integrate_elis(self,
