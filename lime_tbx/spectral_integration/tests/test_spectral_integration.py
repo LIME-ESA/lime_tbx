93454f661faef091819b151ea288a5fb10bc26e7
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-08-24 14:34:51 +0200

    added a test for spectral_integration

--- lime_tbx/spectral_integration/tests/test_spectral_integration.py
+++ lime_tbx/spectral_integration/tests/test_spectral_integration.py
@@ -5,6 +5,7 @@
 
 """___Third-Party Modules___"""
 import unittest
+import numpy as np
 
 """___LIME_TBX Modules___"""
 from ..spectral_integration import ISpectralIntegration, SpectralIntegration
@@ -16,13 +17,26 @@ __maintainer__ = "Javier Gatón Herguedas"
 __email__ = "gaton@goa.uva.es"
 __status__ = "Development"
 
+CH_WLENS = np.array([350, 400, 450, 500])
+CH_SRF = np.array([0.2, 0.2, 0.3, 0.3])
+CH_ELIS = np.array([0.005, 0.0002, 0.3, 0.0001])
+
 
 def get_spectral_integrator() -> ISpectralIntegration:
     return SpectralIntegration()
 
 
 class TestSpectralIntegration(unittest.TestCase):
-    def test_function1(self):
+
+    # Function _convolve_srf
+    def test__convolve_srf_ok(self):
+        si: SpectralIntegration = get_spectral_integrator()
+        signal = si._convolve_srf(CH_WLENS, CH_SRF, CH_ELIS)
+        ch_signal = np.trapz(CH_SRF * CH_ELIS, CH_WLENS)
+        self.assertEqual(signal, ch_signal)
+
+    # Function integrate_elis
+    def test_integrate_elis_ok(self):
         pass
 
 
