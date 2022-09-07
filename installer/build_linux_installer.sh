18344e929e58e65b316c18bb8ebcc0402159c4bc
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-09-07 13:21:11 +0200

    improved linux installer

--- installer/build_linux_installer.sh
+++ installer/build_linux_installer.sh
@@ -2,3 +2,5 @@ mkdir -p linux/installer_files
 [ ! -d "linux/installer_files/kernels" ] && cp -r ../kernels linux/installer_files
 [ ! -d "linux/installer_files/eocfi_data" ] && cp -r ../eocfi_data linux/installer_files
 cp ../dist/LIME_TBX linux/installer_files
+cp ../lime_tbx/gui/assets/lime_logo.png linux/installer_files
+cp linux/limetbx.desktop linux/installer_files
--- installer/linux/lime_installer.sh
+++ installer/linux/lime_installer.sh
@@ -6,6 +6,7 @@ local_appdata="$user_home/.LimeTBX"
 executable="LIME_TBX"
 bin_path="/usr/bin"
 command_name="lime"
+desktop_applications="/usr/share/applications"
 mkdir -p $local_appdata/kernels
 mkdir -p $local_appdata/eocfi_data
 sudo chmod 777 $local_appdata
@@ -14,5 +15,6 @@ sudo chmod 777 $local_appdata/eocfi_data
 mkdir -p $dst
 cp -r $assets/* $dst
 ln -s $dst/$executable $bin_path
+ln -s $dst/limetbx.desktop $desktop_applications
 mv $bin_path/$executable $bin_path/$command_name
 echo "LIME Toolbox installed successfully."
