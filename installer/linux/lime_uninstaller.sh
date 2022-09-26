0707e60d791e65d3c4af36fe126e79b0b90db7a5
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-09-26 14:48:15 +0200

    added lime uninstaller for linux

---
+++ installer/linux/lime_uninstaller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+assets="installer_files"
+dst="/opt/esa/LimeTBX"
+user_home=$(bash -c "cd ~$(printf %q $SUDO_USER) && pwd")
+local_appdata="$user_home/.LimeTBX"
+executable="LIME_TBX"
+bin_path="/usr/bin"
+command_name="lime"
+desktop_applications="/usr/share/applications"
+rm -rf $local_appdata
+rm $bin_path/$command_name
+rm $desktop_applications/limetbx.desktop
+rm -rf $dst
+echo "LIME Toolbox uninstalled successfully."
