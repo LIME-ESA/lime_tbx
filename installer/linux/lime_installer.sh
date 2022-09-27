8ddef8a90bf5475fd3c8b33fae067f846017b2b3
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-09-27 11:14:23 +0200

    improved lime installer for linux

--- installer/linux/lime_installer.sh
+++ installer/linux/lime_installer.sh
@@ -9,13 +9,13 @@ command_name="lime"
 desktop_applications="/usr/share/applications"
 mkdir -p $local_appdata/kernels
 mkdir -p $local_appdata/eocfi_data
-sudo chmod 777 $local_appdata
-sudo chmod 777 $local_appdata/kernels
-sudo chmod 777 $local_appdata/eocfi_data
+chmod 777 $local_appdata
+chmod 777 $local_appdata/kernels
+chmod 777 $local_appdata/eocfi_data
 mkdir -p $dst
 cp -r $assets/* $dst
 ln -s $dst/$executable $bin_path
 ln -s $dst/limetbx.desktop $desktop_applications
 mv $bin_path/$executable $bin_path/$command_name
-sudo chmod -R +rx $dst
+chmod -R +rx $dst
 echo "LIME Toolbox installed successfully."
