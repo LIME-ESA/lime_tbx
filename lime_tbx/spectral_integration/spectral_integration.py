fix custom kernel and spectral integration normalisation

lime_tbx/spectral_integration/spectral_integration.py
@@ -60,7 +60,7 @@ class SpectralIntegration(ISpectralIntegration):
         """
         Convolve ch_srf and ch_elis over ch_wlens, generating the signal.
         """
-        ch_signal = np.trapz(ch_srf * ch_elis, ch_wlens)
+        ch_signal = np.trapz(ch_srf * ch_elis, ch_wlens)/np.trapz(ch_srf, ch_wlens)
         return ch_signal
 
     def integrate_elis(