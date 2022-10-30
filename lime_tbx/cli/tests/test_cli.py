75c11b009b21003d25ddaa388e89dda159bba30a
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-10-30 13:26:03 +0100

    cli unittests checking for errorcode

--- lime_tbx/cli/tests/test_cli.py
+++ lime_tbx/cli/tests/test_cli.py
@@ -55,123 +55,138 @@ class TestCLI_CaptureSTDOUTERR(unittest.TestCase):
     def test_get_help(self):
         cli = get_cli()
         args = "-h"
-        cli.handle_input(get_opts(args))
+        errcode = cli.handle_input(get_opts(args))
+        self.assertEqual(errcode, 0)
         f = open("./test_files/cli/help.txt")
         self.assertEqual(self.capturedOutput.getvalue(), f.read())
         f.close()
 
     def test_sat_err_date(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-s PROBA-V,2023-01-20T02:00:00 -o graph,png,ignore_folder/refl,ignore_folder/irr,ignore_folder/polar"
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/sat_err_date.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_sat_err_date_timeseries(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-s ENVISAT -t ./test_files/csv/timeseries.csv -o graph,png,ignore_folder/refl,ignore_folder/irr,ignore_folder/polar"
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/sat_err_date.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_sat_err_graphd(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-s PROBA-V,2020-01-20T02:00:00 -o graphd,png,ignore_folder/refl,ignore_folder/irr,ignore_folder/polar"
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/sat_err_graphd.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_sat_probav_csv_missing_arg(self):
         cli = get_cli()
-        cli.handle_input(get_opts("-s PROBA-V,2020-01-20T02:00:00 -o csv,p1,p2,p3"))
+        errcode = cli.handle_input(
+            get_opts("-s PROBA-V,2020-01-20T02:00:00 -o csv,p1,p2,p3")
+        )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_num_args_o_csv.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_sat_probav_csv_extra_arg(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts("-s PROBA-V,2020-01-20T02:00:00 -o csv,p1,p2,p3,p4,p5")
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_num_args_o_csv.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_sat_probav_missing_datetime(self):
         cli = get_cli()
-        cli.handle_input(get_opts("-s PROBA-V -o csv,p1,p2,p3,p4"))
+        errcode = cli.handle_input(get_opts("-s PROBA-V -o csv,p1,p2,p3,p4"))
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_miss_datetime_sat.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_earth_glod_missing_path(self):
         cli = get_cli()
-        cli.handle_input(get_opts("-e 80,80,2000,2010-10-1T02:02:02 -o nc"))
+        errcode = cli.handle_input(get_opts("-e 80,80,2000,2010-10-1T02:02:02 -o nc"))
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_miss_o_nc_path.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_earth_glod_missing_datetime(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts("-e 80,80,2000 -o nc,./test_files/cli/cliglod.test.nc")
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_miss_e_datetime.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_lunar_missing_arg(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-l 0.98,420000,20.5,-30.2,0.69 -o nc,test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_wrong_l_args.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_lunar_extra_arg(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-l 0.98,420000,20.5,-30.2,0.69,15,2 -o nc,test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_wrong_l_args.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_comparison_no_observations(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,BOTH,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_c_no_obs.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
 
     def test_comparison_glob_csvd_wrong_mpa_dt(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT*" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,BATH,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 1)
         f = open("./test_files/cli/err_c_o_mpa_dt_both_wrong.txt")
         self.assertEqual(self.capturedErr.getvalue(), f.read())
         f.close()
@@ -183,35 +198,39 @@ class TestCLI(unittest.TestCase):
 
     def test_earth_glod_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-e 80,80,2000,2010-10-1T02:02:02 -o nc,./test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_earth_timeseries_glod_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-e 80,80,2000 -t ./test_files/csv/timeseries.csv -o nc,./test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_earth_timeseries_with_datetime_glod_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-e 80,80,2000,2010-10-1T02:02:02 -t ./test_files/csv/timeseries.csv -o nc,./test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_lunar_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-l 0.98,420000,20.5,-30.2,0.69,15 -o nc,test_files/cli/cliglod.test.nc"
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_sat_probav_csv(self):
         path_refl = "./test_files/cli/proba_refl.test.csv"
@@ -219,13 +238,14 @@ class TestCLI(unittest.TestCase):
         path_polar = "./test_files/cli/proba_polar.test.csv"
         path_integrated = "./test_files/cli/proba_integrated.test.csv"
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-s PROBA-V,2020-01-20T02:00:00 -o csv,{},{},{},{}".format(
                     path_refl, path_irr, path_polar, path_integrated
                 )
             )
         )
+        self.assertEqual(errcode, 0)
         # This method doesnt work because interpolation gives different results each time
         """
         self.assertTrue(filecmp.cmp(path_refl, "./test_files/cli/proba_refl.csv"))
@@ -239,11 +259,12 @@ class TestCLI(unittest.TestCase):
         path_irr = "./test_files/cli/proba_irr.test.png"
         path_polar = "./test_files/cli/proba_polar.test.png"
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 "-s PROBA-V,2020-01-20T02:00:00 -o graph,png,test_files/cli/proba_refl.test,test_files/cli/proba_irr.test,test_files/cli/proba_polar.test"
             )
         )
+        self.assertEqual(errcode, 0)
         self.assertTrue(os.path.exists(path_refl))
         self.assertTrue(os.path.exists("./test_files/cli/proba_refl.png"))
         self.assertTrue(os.path.exists(path_irr))
@@ -253,19 +274,21 @@ class TestCLI(unittest.TestCase):
 
     def test_comparison_glob_csvd_both_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT*" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,BOTH,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_comparison_glob_graphd_png_both_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT*" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o graphd,png,BOTH,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 0)
         graphs = [
             "NIR016.dt.png",
             "NIR016.mpa.png",
@@ -282,24 +305,27 @@ class TestCLI(unittest.TestCase):
 
     def test_comparison_glob_csvd_dt_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT*" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,DT,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_comparison_glob_csvd_mpa_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT*" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,MPA,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 0)
 
     def test_comparison_csvd_both_ok(self):
         cli = get_cli()
-        cli.handle_input(
+        errcode = cli.handle_input(
             get_opts(
                 '-c "lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT-Darmstadt,VISNIR+SUBSET+MOON,MSG3+SEVIRI_C_EUMG_20130101145644_01.nc lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT-Darmstadt,VISNIR+SUBSET+MOON,MSG3+SEVIRI_C_EUMG_20140318140112_01.nc, lime_tbx/filedata/sample_moon_data/W_XX-EUMETSAT-Darmstadt,VISNIR+SUBSET+MOON,MSG3+SEVIRI_C_EUMG_20140715153303_01.nc" -f lime_tbx/filedata/sample_data/W_XX-EUMETSAT-Darmstadt_VIS+IR+SRF_MSG3+SEVIRI_C_EUMG.nc -o csvd,BOTH,test_files/cli/comp_out.test.dir/'
             )
         )
+        self.assertEqual(errcode, 0)
