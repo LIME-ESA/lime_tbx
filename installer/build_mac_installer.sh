8065e0f38390badf33f12254f7b0dec2857c9161
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-11-03 02:09:11 -0700

    changed mac installer maker

--- installer/build_mac_installer.sh
+++ installer/build_mac_installer.sh
@@ -1,8 +1,10 @@
-mkdir -p mac/installer_files
-[ ! -d "mac/scripts/installer_files/kernels" ] && cp -r ../kernels mac/installer_files
-[ ! -d "mac/scripts/installer_files/eocfi_data" ] && cp -r ../eocfi_data mac/installer_files
-cp -r ../dist/LimeTBX.app mac/bundle
-mv mac/installer_files/* mac/bundle/LimeTBX.app/Contents/Resources
+mkdir -p mac/temp_files
+[ ! -d "mac/temp_files/kernels" ] && cp -r ../kernels mac/temp_files
+[ ! -d "mac/temp_files/eocfi_data" ] && cp -r ../eocfi_data mac/temp_files
+mkdir -p mac/bundle
+cp -r ../dist/LimeTBX.app mac/bundle/LimeTBX.app
+mv mac/temp_files/* mac/bundle/LimeTBX.app/Contents/Resources
+rmdir mac/temp_files
 #zip -r mac/lime_installer.zip mac/installer_files mac/lime_installer.sh mac/lime_uninstaller.sh
 version='0.0.2'
 pkgbuild --install-location /Applications --root mac/bundle --identifier 'int.esa.lime' --version $version mac/lime.pkg
