c5e48fb206ecb3959165c24f014fc448cb51a9aa
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-11-03 04:58:57 -0700

    mac installer generated .pkg file

--- installer/build_mac_installer.sh
+++ installer/build_mac_installer.sh
@@ -1,10 +1,8 @@
-mkdir -p mac/temp_files
-[ ! -d "mac/temp_files/kernels" ] && cp -r ../kernels mac/temp_files
-[ ! -d "mac/temp_files/eocfi_data" ] && cp -r ../eocfi_data mac/temp_files
+[ -d "mac/bundle" ] && rm -rf mac/bundle
+[ -d "mac/lime.pkg" ] && rm -rf mac/lime.pkg
 mkdir -p mac/bundle
 cp -r ../dist/LimeTBX.app mac/bundle/LimeTBX.app
-mv mac/temp_files/* mac/bundle/LimeTBX.app/Contents/Resources
-rmdir mac/temp_files
-#zip -r mac/lime_installer.zip mac/installer_files mac/lime_installer.sh mac/lime_uninstaller.sh
+cp -r ../kernels mac/bundle/LimeTBX.app/Contents/Resources/kernels
+cp -r ../eocfi_data mac/bundle/LimeTBX.app/Contents/Resources/eocfi_data
 version='0.0.2'
-#pkgbuild --install-location /Applications --root mac/bundle --identifier 'int.esa.LimeTBX' --version $version mac/lime.pkg
+pkgbuild --install-location /Applications --root mac/bundle --identifier 'int.esa.LimeTBX' --scripts mac/scripts --version $version mac/lime.pkg
