4c25f46278105f7acef862061c1d02b182ff1b1e
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-11-07 09:11:48 +0100

    fixed linux installer generator, zip is previously removed

--- installer/build_linux_installer.sh
+++ installer/build_linux_installer.sh
@@ -2,7 +2,7 @@ mkdir -p linux/installer_files
 [ ! -d "linux/installer_files/kernels" ] && cp -r ../kernels linux/installer_files
 [ ! -d "linux/installer_files/eocfi_data" ] && cp -r ../eocfi_data linux/installer_files
 [ -d "linux/installer_files/LimeTBX" ] && rm -rf linux/installer_files/LimeTBX
-[ -d "linux/lime_installer.zip" ] && rm -f linux/lime_installer.zip
+[ -f "linux/lime_installer.zip" ] && rm -f linux/lime_installer.zip
 cp -r ../dist/LimeTBX linux/installer_files
 cp ../lime_tbx/gui/assets/lime_logo.png linux/installer_files
 cp linux/limetbx.desktop linux/installer_files
