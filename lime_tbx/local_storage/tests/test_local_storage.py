98ffcfc859922caded65d754a0d326e9235be85e
Javier Gatón Herguedas <gaton@goa.uva.es>
2023-01-26 13:54:50 +0100

    added tests for local_storage

--- lime_tbx/local_storage/tests/test_local_storage.py
+++ lime_tbx/local_storage/tests/test_local_storage.py
@@ -1,13 +1,14 @@
 """Tests for classname module"""
 
 """___Built-In Modules___"""
-# import here
+import logging
+import sys
 
 """___Third-Party Modules___"""
 import unittest
 
-"""___NPL Modules___"""
-# import here
+"""___LIME TBX Modules___"""
+from .. import appdata, programdata
 
 """___Authorship___"""
 __author__ = "Pieter De Vis"
@@ -17,9 +18,31 @@ __email__ = "pieter.de.vis@npl.co.uk"
 __status__ = "Development"
 
 
+def get_logger() -> logging.Logger:
+    test_logger = logging.getLogger("test_logger")
+    handler = logging.StreamHandler(None)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    test_logger.addHandler(handler)
+    return test_logger
+
+
 class TestLocalStorage(unittest.TestCase):
-    def test_function1(self):
-        pass
+    def test_is_valid_appdata_repo_ok(self):
+        self.assertTrue(appdata._is_valid_appdata(".", get_logger()))
+
+    def test_is_not_valid_appdata_repo_coeff_data(self):
+        logger = get_logger()
+        logger.disabled = True
+        self.assertFalse(appdata._is_valid_appdata("/", logger))
+
+    def test_is_valid_programfiles_repo_ok(self):
+        self.assertTrue(programdata._is_valid_programfiles("."))
+
+    def test_is_not_valid_programfiles_repo_coeff_data(self):
+        self.assertFalse(programdata._is_valid_programfiles("./coeff_data"))
 
 
 if __name__ == "__main__":
