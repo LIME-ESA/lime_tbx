5d05bd7771dcd7a73cedd6eefa2c852e44915f18
Javier Gatón Herguedas <gaton@goa.uva.es>
2023-04-12 11:02:12 +0200

    updated version to 0.1.1

--- installer/debian/build_deb.sh
+++ installer/debian/build_deb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Building lime .deb package."
-# 0.1.0 Comment so the search for the version a.b.c appears (here it is a.b-c)
-# Change it in the control file too!
-version="0.1-0"
+# 0.1.1 Comment so the search for the version a.b.c appears (here it is a.b-c)
+version="0.1-1"
+# 0.1.1 Change it in the control file too!
 name="lime_"$version
 [ -d $name ] && rm -rf $name
 [ -f $name".deb" ] && rm -f $name".deb"
