08bc4fa841771f98939e90b6d4ba0866aa04e7e8
Javier Gatón Herguedas <gaton@goa.uva.es>
2023-12-12 14:06:17 +0100

    removed ld coeff version and fixed current polarization uncertainty coeffs

--- lime_tbx/cli/tests/test_cli.py
+++ lime_tbx/cli/tests/test_cli.py
@@ -485,7 +485,7 @@ class TestCLI(unittest.TestCase):
         cli = get_cli()
         errcode = cli.handle_input(
             get_opts(
-                "-e 80,80,2000,2010-10-1T02:02:02 -o nc,./test_files/cli/cliglod.test.nc -C 20230123_v1"
+                "-e 80,80,2000,2010-10-1T02:02:02 -o nc,./test_files/cli/cliglod.test.nc -C 20231120_v1"
             )
         )
         self.assertEqual(errcode, 0)
