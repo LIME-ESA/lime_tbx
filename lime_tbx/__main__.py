0f457621e7d1c49e1e529d44c285b5f953fb23db
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-04-08 17:26:32 +0200

    removed commented code that avoided warnings of non symmetric matrices (they have to be printed)

--- lime_tbx/__main__.py
+++ lime_tbx/__main__.py
@@ -86,18 +86,6 @@ def main():
     logger.get_logger().info("ToolBox started")
     warnings.filterwarnings("ignore", ".*Gtk-WARNING.*")
     warnings.filterwarnings("ignore", ".*Fontconfig warning.*")
-    # TODO remove the following warning filters. It shouldn't happen, if it happens print warning
-    # so it'll be fixed.
-    # warnings.filterwarnings(
-    #    "ignore",
-    #    ".*One of the provided covariance matrix is not positive.*",
-    #    UserWarning,
-    # )
-    # warnings.filterwarnings(
-    #    "ignore",
-    #    ".*One of the correlation matrices is not positive.*",
-    #    UserWarning,
-    # )
     warnings.filterwarnings(
         "ignore",
         ".*elementwise comparison failed; returning scalar instead.*",
