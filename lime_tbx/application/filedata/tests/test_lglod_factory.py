1a5dbae7b8e911138642bf4fdfdb63ab54077251
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-09-04 11:00:36 +0200

    multiseleno lglod export import integration tests

--- lime_tbx/application/filedata/tests/test_lglod_factory.py
+++ lime_tbx/application/filedata/tests/test_lglod_factory.py
@@ -8,7 +8,7 @@ import unittest
 import numpy as np
 
 """___LIME_TBX Modules___"""
-from .. import lglod_factory
+from .. import lglod_factory, lglod as lglodlib, srf as srflib
 from lime_tbx.application.simulation.lime_simulation import LimeSimulation
 from lime_tbx.common.datatypes import (
     KernelsPath,
@@ -17,6 +17,7 @@ from lime_tbx.common.datatypes import (
     CustomPoint,
     MultipleCustomPoint,
     EocfiPath,
+    LGLODData,
 )
 from lime_tbx.business.spectral_integration.spectral_integration import get_default_srf
 from lime_tbx.business.lime_algorithms.lime.tests.test_elref import get_coeffs
@@ -142,6 +143,91 @@ class TestLGLODFactory(unittest.TestCase):
         np.testing.assert_array_equal(
             lglod.elis_cimel[1].data, ls.get_elis_cimel()[1].data
         )
+
+
+class TestLGLODFactoryLGLODIntegration(unittest.TestCase):
+    def assert_eq_lglod(self, a: LGLODData, b: LGLODData):
+        self.assertEqual(len(a.elis_cimel), len(b.elis_cimel))
+        for i, (aec, bec) in enumerate(zip(a.elis_cimel, b.elis_cimel)):
+            np.testing.assert_array_equal(
+                aec.data,
+                bec.data,
+                f"Mismatch in elis_cimel.data at {i}.\n{aec.data}\n{bec.data}",
+            )
+            np.testing.assert_array_equal(
+                aec.wlens,
+                bec.wlens,
+                f"Mismatch in elis_cimel.wlens at {i}\n{aec.wlens}\n{bec.wlens}",
+            )
+        np.testing.assert_array_equal(a.signals.wlens, b.signals.wlens)
+        np.testing.assert_array_equal(a.signals.data, b.signals.data)
+
+    def test_createlglod_write_read_write_multiseleno(self):
+        ls = get_lime_simulation()
+        srf = get_default_srf()
+        pts = [
+            CustomPoint(1, 400000, 30, 30, 1, 40, -40),
+            CustomPoint(1.00002, 400001, 32, 31, 2, 41, -41),
+        ]
+        pt = MultipleCustomPoint(pts)
+        ls.update_irradiance(srf, srf, pt, get_coeffs())
+        ls.update_polarisation(srf, pt, POL_COEFFS)
+        mdas = ls.get_moon_datas()
+        lglod = lglod_factory.create_lglod_data(
+            pt,
+            srf,
+            ls,
+            KERNELS_PATH,
+            "test",
+            "testp",
+            "testa",
+            "test",
+            mdas,
+        )
+        path = "./test_files/moon/sim_multiseleno.e2e.test.nc"
+        now = datetime.now(timezone.utc)
+        inside_mpa_range = [True for _ in range(len(lglod.elis_cimel))]
+        lglodlib.write_obs(lglod, path, now, inside_mpa_range)
+        rlglod = lglodlib.read_lglod_file(path, KERNELS_PATH)
+        self.assert_eq_lglod(lglod, rlglod)
+        path = "./test_files/moon/sim_multiseleno2.e2e.test.nc"
+        lglodlib.write_obs(rlglod, path, now, inside_mpa_range)
+        lglod = lglodlib.read_lglod_file(path, KERNELS_PATH)
+        self.assert_eq_lglod(rlglod, lglod)
+
+    def test_createlglod_write_read_write_multiseleno_cimel(self):
+        ls = get_lime_simulation()
+        srf = get_default_srf()
+        sigsrf = srflib.read_srf("./test_files/moon/cimel_srf.nc")
+        pts = [
+            CustomPoint(1, 400000, 30, 30, 1, 40, -40),
+            CustomPoint(1.00002, 400001, 32, 31, 2, 41, -41),
+        ]
+        pt = MultipleCustomPoint(pts)
+        ls.update_irradiance(srf, sigsrf, pt, get_coeffs())
+        ls.update_polarisation(srf, pt, POL_COEFFS)
+        mdas = ls.get_moon_datas()
+        lglod = lglod_factory.create_lglod_data(
+            pt,
+            sigsrf,
+            ls,
+            KERNELS_PATH,
+            "test",
+            "testp",
+            "testa",
+            "test",
+            mdas,
+        )
+        path = "./test_files/moon/sim_multiseleno_cimel.e2e.test.nc"
+        now = datetime.now(timezone.utc)
+        inside_mpa_range = [True for _ in range(len(lglod.elis_cimel))]
+        lglodlib.write_obs(lglod, path, now, inside_mpa_range)
+        rlglod = lglodlib.read_lglod_file(path, KERNELS_PATH)
+        self.assert_eq_lglod(lglod, rlglod)
+        path = "./test_files/moon/sim_multiseleno_cimel2.e2e.test.nc"
+        lglodlib.write_obs(rlglod, path, now, inside_mpa_range)
+        lglod = lglodlib.read_lglod_file(path, KERNELS_PATH)
+        self.assert_eq_lglod(rlglod, lglod)
 
 
 if __name__ == "__main__":
