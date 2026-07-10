aa69f1177c6963c55c4095c86a6df9c915fff514
Javier Gatón Herguedas <gaton@goa.uva.es>
2026-07-10 14:15:39 +0200

    Refactor and fix EO‑CFI satellite position calculation
    - Extract common date‑to‑position logic into compute_position_for_date()
    - Fix propagation model for TLE: use XO_PROPAG_MODEL_TLE (was MEAN_KEPL)
    - Add validity check (xo_orbit_get_osv_compute_validity) to reject out‑of‑range times
    - Improve error handling: return immediately on time/orbit init failures
    - Fix const‑correctness warnings with explicit casts in library calls
    - Add input validation (NULL checks) for pointers
    - Ensure memory is freed on all error paths in main
    - Remove unused variables (vstop2, val_time1, extra_choice, etc.)
    - Check return status of get_satellite_position_* in main and report errors

--- lime_tbx/business/eocfi_adapter/eocfi_c/code/get_positions.c
+++ lime_tbx/business/eocfi_adapter/eocfi_c/code/get_positions.c
@@ -11,6 +11,11 @@
 #define XO_MAX_STR_LENGTH        256
 #define DEFAULT_TERMINATE_CHECK_STATUS 1
 
+typedef enum {
+    INPUT_TYPE_OSF = 0,
+    INPUT_TYPE_TLE = 1
+} InputType;
+
 #define DEFINE_CHECK_STATUS(xx, XX)                                                                 \
   static void check_##xx##_status(long func, long status, const long *err, int terminate) {         \
     if (status != XX##_OK)                                                                          \
@@ -44,51 +49,33 @@ static const int monthDays[12]
 static int countLeapYears(struct Date d)
 {
     int years = d.y;
-
-    // Check if the current year needs to be
-    //  considered for the count of leap years
-    // or not
     if (d.m <= 2)
         years--;
-
-    // An year is a leap year if it
-    // is a multiple of 4,
-    // multiple of 400 and not a
-    // multiple of 100.
     return years / 4
            - years / 100
            + years / 400;
 }
 
-// This function returns number of
-// days between two given dates
 static int getDifference(struct Date dt)
 {
     struct Date dt2 = dt;
     struct Date dt1 = {2000, 1, 1, 00, 00, 00, 00};
-    // COUNT TOTAL NUMBER OF DAYS
-    // BEFORE FIRST DATE 'dt1'
 
-    // initialize count using years and day
+    if (dt.y < 2000) {
+        fprintf(stderr, "Error: Date before year 2000 not supported\n");
+        return -1;
+    }
+
     long int n1 = dt1.y * 365 + dt1.d;
-
-    // Add days for months in given date
     for (int i = 0; i < dt1.m - 1; i++)
         n1 += monthDays[i];
-
-    // Since every leap year is of 366 days,
-    // Add a day for every leap year
     n1 += countLeapYears(dt1);
-
-    // SIMILARLY, COUNT TOTAL NUMBER OF
-    // DAYS BEFORE 'dt2'
 
     long int n2 = dt2.y * 365 + dt2.d;
     for (int i = 0; i < dt2.m - 1; i++)
         n2 += monthDays[i];
     n2 += countLeapYears(dt2);
 
-    // return difference between two counts
     return (n2 - n1);
 }
 
@@ -107,6 +94,70 @@ static char* to_timestamp(const xl_time_id *time, double instant) {
     return buffer;
 }
 
+static int compute_position_for_date(
+        const xo_orbit_id *orbit_id,
+        const xl_time_id *time_id,
+        const struct Date *dt,
+        long propag_model,
+        double *pos,
+        double *vel,
+        double *acc,
+        long *errors)
+{
+    char msg[XO_MAX_COD][XO_MAX_STR];
+    long n = 0;
+    double seconds = (dt->hh * 3600) + (dt->mm * 60) + dt->ss + dt->microsecs / 1000000.0;
+    long diff_days = getDifference(*dt);
+    if (diff_days < 0) return XL_ERR;  // invalid date
+
+    double vstart2 = diff_days + seconds / 86400.0;
+    double val_time0 = vstart2;
+
+#ifdef DEBUG
+    printf("\n\t-  validity time = ( %f )", val_time0);
+#endif
+
+    double time = val_time0;
+
+    xo_validity_time val_time;
+    long status = xo_orbit_get_osv_compute_validity(orbit_id, &val_time);
+    if (status != XO_OK) {
+        fprintf(stderr, "xo_orbit_get_osv_compute_validity failed with status %ld\n", status);
+        return status;
+    }
+    if (time < val_time.start || time > val_time.stop) {
+        fprintf(stderr, "Requested time %f is outside orbit validity [%f, %f]\n", time, val_time.start, val_time.stop);
+        return XL_ERR;
+    }
+
+    long time_ref_utc = XO_TIME_UTC;
+
+    status = xo_osv_compute(orbit_id, &propag_model, &time_ref_utc, &time,
+                            pos, vel, acc, errors);
+
+    if (status != XO_OK) {
+        long func_id = XO_OSV_COMPUTE_ID;
+        xo_get_msg(&func_id, errors, &n, msg);
+        xo_print_msg(&n, msg);
+        return status;
+    }
+
+#ifdef DEBUG
+    printf( "\n\t-  time = %lf (%s)", time,  to_timestamp(time_id, time));
+    printf( "\n\t-  pos[0] = %lf", pos[0] );
+    printf( "\n\t-  pos[1] = %lf", pos[1] );
+    printf( "\n\t-  pos[2] = %lf", pos[2] );
+    printf( "\n\t-  vel[0] = %lf", vel[0] );
+    printf( "\n\t-  vel[1] = %lf", vel[1] );
+    printf( "\n\t-  vel[2] = %lf", vel[2] );
+    printf( "\n\t-  acc[0] = %lf", acc[0] );
+    printf( "\n\t-  acc[1] = %lf", acc[1] );
+    printf( "\n\t-  acc[2] = %lf", acc[2] );
+#endif
+
+    // Extra computation not used - removed to avoid unused vars
+    return XL_OK;
+}
 
 #ifdef _WIN32
 __declspec(dllexport) int  get_satellite_position_osf(
@@ -116,7 +167,7 @@ __declspec(dllexport) int  get_satellite_position_osf(
         long sat_id,
         int quant_dates,
         int** dates,
-        char* orbit_file,
+        const char* orbit_file,
         double** position_returns
     ){
     char msg[XO_MAX_COD][XO_MAX_STR];
@@ -124,36 +175,21 @@ __declspec(dllexport) int  get_satellite_position_osf(
     long one = 1;
     long func_id;
 
-    // Initialize time, based on the correlation provided with the orbit
     xl_time_id time_id = {NULL};
-    double vstart, vstop;  // Validity start/stop, decimal days from 2000-01-01T00:00:00.000000 UTC
-    double vstart2, vstop2;  // Validity start/stop, decimal days from 2000-01-01T00:00:00.000000 UTC
+    double vstart, vstop;
 
-    /* xl_time_ref_init_file */
     long   trif_time_model, trif_time_init_mode, trif_time_ref ;
     long   errors[XO_ERR_VECTOR_MAX_LENGTH], status;
 
-    /* xo_orbit_init_file */
     xo_orbit_id    orbit_id    = {NULL};
     xl_model_id    model_id    = {NULL};
     long time_init_mode, orbit_mode;
     long orbit0, orbit1;
-    double val_time0, val_time1, time;
-    xo_validity_time val_time;
+    double val_time0, val_time1;
 
-    /* variables for xo_osv_compute_extra */
-
-    long extra_choice;
-    double orbit_model_out[XO_ORBIT_EXTRA_NUM_DEP_ELEMENTS],
-            orbit_extra_out[XO_ORBIT_EXTRA_NUM_INDEP_ELEMENTS];
-
-    /* common variables */
-    long propag_model = XO_PROPAG_MODEL_MEAN_KEPL;
     long time_ref_utc = XO_TIME_UTC;
 
-    double pos[3];
-    double vel[3];
-    double acc[3];
+    double pos[3], vel[3], acc[3];
 
 #ifdef DEBUG
     xl_verbose();
@@ -162,15 +198,18 @@ __declspec(dllexport) int  get_satellite_position_osf(
     xf_verbose();
 #endif
 
-    /* Time Initialization */
-    /* ------------------- */
+    // Input validation
+    if (dates == NULL || position_returns == NULL || orbit_file == NULL) {
+        return XL_ERR;
+    }
 
+    /* Time Initialization */
     trif_time_model     = XL_TIMEMOD_AUTO;
     trif_time_init_mode = XL_SEL_FILE;
     trif_time_ref       = XL_TIME_UTC;
 
     status = xl_time_ref_init_file(&trif_time_model, &one,
-                                   &orbit_file,
+                                   (char**)&orbit_file,
                                    &trif_time_init_mode, &trif_time_ref, NULL,
                                    NULL, NULL, NULL, &vstart, &vstop, &time_id, errors);
 
@@ -179,17 +218,15 @@ __declspec(dllexport) int  get_satellite_position_osf(
         func_id = XL_TIME_REF_INIT_FILE_ID;
         xl_get_msg(&func_id, errors, &n, msg);
         xl_print_msg(&n, msg);
+        return XL_ERR;
     }
 
     /* Orbit initialization */
-    /* -------------------- */
-
     time_init_mode = XO_SEL_FILE;
-
     orbit_mode = XO_ORBIT_INIT_AUTO;
 
     status =  xo_orbit_init_file(&sat_id, &model_id, &time_id,
-                                 &orbit_mode, &one, &orbit_file,
+                                 &orbit_mode, &one, (char**)&orbit_file,
                                  &time_init_mode, &time_ref_utc,
                                  &vstart, &vstop, &orbit0, &orbit1,
                                  &val_time0, &val_time1, &orbit_id,
@@ -200,54 +237,17 @@ __declspec(dllexport) int  get_satellite_position_osf(
         func_id = XO_ORBIT_INIT_FILE_ID;
         xo_get_msg(&func_id, errors, &n, msg);
         xo_print_msg(&n, msg);
+        return XL_ERR;
     }
 
-    status = xo_orbit_get_osv_compute_validity(&orbit_id, &val_time);
-
+    // Loop over dates
     for (int i = 0; i < quant_dates; i++){
-        struct Date dt = {dates[i][0], dates[i][1], dates[i][2], dates[i][3], dates[i][4], dates[i][5], dates[i][6]};
-        double seconds = (dt.hh * 3600) + (dt.mm * 60) + dt.ss + dt.microsecs/1000000.0;
-        double start_time     = getDifference(dt);
-        vstart2 = start_time + seconds / 86400.;
-        vstop2 = vstart2 + 0.5;
-        val_time0 = vstart2;
-        val_time1 = vstop2;
-#ifdef DEBUG
-        printf("\n\t-  validity times = ( %f , %f )", val_time0, val_time1 );
-#endif
-        time = val_time0;
+        struct Date dt = {dates[i][0], dates[i][1], dates[i][2],
+                          dates[i][3], dates[i][4], dates[i][5], dates[i][6]};
 
-        status = xo_osv_compute(&orbit_id, &propag_model, &time_ref_utc, &time,
-                /* outputs */
-                                pos, vel, acc, errors);
-
-        if (status != XO_OK)
-        {
-            func_id = XO_OSV_COMPUTE_ID;
-            xo_get_msg(&func_id, errors, &n, msg);
-            xo_print_msg(&n, msg);
-        }
-#ifdef DEBUG
-        printf( "\n\t-  time = %lf (%s)", time,  to_timestamp(&time_id, time));
-        printf( "\n\t-  pos[0] = %lf", pos[0] );
-        printf( "\n\t-  pos[1] = %lf", pos[1] );
-        printf( "\n\t-  pos[2] = %lf", pos[2] );
-        printf( "\n\t-  vel[0] = %lf", vel[0] );
-        printf( "\n\t-  vel[1] = %lf", vel[1] );
-        printf( "\n\t-  vel[2] = %lf", vel[2] );
-        printf( "\n\t-  acc[0] = %lf", acc[0] );
-        printf( "\n\t-  acc[1] = %lf", acc[1] );
-        printf( "\n\t-  acc[2] = %lf", acc[2] );
-#endif
-        extra_choice = XO_ORBIT_EXTRA_NO_RESULTS;
-
-        status = xo_osv_compute_extra(&orbit_id, &extra_choice,
-                                        orbit_model_out, orbit_extra_out, errors);
-        if (status != XO_OK)
-        {
-            func_id = XO_OSV_COMPUTE_EXTRA_ID;
-            xo_get_msg(&func_id, errors, &n, msg);
-            xo_print_msg(&n, msg);
+        status = compute_position_for_date(&orbit_id, &time_id, &dt, XO_PROPAG_MODEL_MEAN_KEPL, pos, vel, acc, errors);
+        if (status != XL_OK) {
+            return XL_ERR;
         }
 
         position_returns[i][0] = pos[0];
@@ -256,8 +256,6 @@ __declspec(dllexport) int  get_satellite_position_osf(
     }
 
     /* Calling to xo_orbit_close */
-    /* ------------------------- */
-
     status = xo_orbit_close(&orbit_id, errors);
     if (status != XO_OK)
     {
@@ -278,11 +276,11 @@ __declspec(dllexport) int  get_satellite_position_tle(
         long sat_id,
         int quant_dates,
         int** dates,
-        char *time_file,
+        const char *time_file,
         long norad,
-        char *sat_name,
-        char *intdes,
-        char *tle_file,
+        const char *sat_name,
+        const char *intdes,
+        const char *tle_file,
         double** position_returns
         ){
 
@@ -303,31 +301,18 @@ __declspec(dllexport) int  get_satellite_position_tle(
     long func_id;
     long one = 1;
 
-    // Initialize time, based on the correlation provided with the orbit
     xl_time_id time_id = {NULL};
-    double vstart, vstop;  // Validity start/stop, decimal days from 2000-01-01T00:00:00.000000 UTC
-    double vstart2, vstop2;  // Validity start/stop, decimal days from 2000-01-01T00:00:00.000000 UTC
+    double vstart, vstop;
 
-    /* xl_time_ref_init_file */
     long   trif_time_model, trif_time_init_mode, trif_time_ref ;
     long   errors[XO_ERR_VECTOR_MAX_LENGTH], status;
 
-    /* xo_orbit_init_file */
     xo_orbit_id    orbit_id    = {NULL};
     xl_model_id    model_id    = {NULL};
     long time_init_mode, orbit_mode;
     long orbit0, orbit1;
-    double val_time0, val_time1, time;
-    xo_validity_time val_time;
+    double val_time0, val_time1;
 
-    /* variables for xo_osv_compute_extra */
-
-    long extra_choice;
-    double orbit_model_out[XO_ORBIT_EXTRA_NUM_DEP_ELEMENTS],
-            orbit_extra_out[XO_ORBIT_EXTRA_NUM_INDEP_ELEMENTS];
-
-    /* common variables */
-    long propag_model = XO_PROPAG_MODEL_MEAN_KEPL;
     long time_ref_utc = XO_TIME_UTC;
 
     double pos[3];
@@ -341,10 +326,12 @@ __declspec(dllexport) int  get_satellite_position_tle(
     xf_verbose();
 #endif
 
+    // Input validation
+    if (dates == NULL || position_returns == NULL || time_file == NULL || tle_file == NULL) {
+        return XL_ERR;
+    }
+
     /* Time Initialization */
-    /* ------------------- */
-
-
     trif_time_model     = XL_TIMEMOD_AUTO;
     trif_time_init_mode = XL_SEL_FILE;
     trif_time_ref       = XL_TIME_UTC;
@@ -367,6 +354,7 @@ __declspec(dllexport) int  get_satellite_position_tle(
         func_id = XL_TIME_REF_INIT_FILE_ID;
         xl_get_msg(&func_id, errors, &n, msg);
         xl_print_msg(&n, msg);
+        return XL_ERR;
     }
 
     /* TLE Initialization */
@@ -380,10 +368,7 @@ __declspec(dllexport) int  get_satellite_position_tle(
     }
 
     /* Orbit initialization */
-    /* -------------------- */
-
     time_init_mode = XO_SEL_FILE;
-
     orbit_mode = XO_ORBIT_INIT_TLE_MODE;
 #ifdef DEBUG
     printf("almost\n");fflush(stdout);
@@ -402,54 +387,17 @@ __declspec(dllexport) int  get_satellite_position_tle(
         func_id = XO_ORBIT_INIT_FILE_ID;
         xo_get_msg(&func_id, errors, &n, msg);
         xo_print_msg(&n, msg);
+        return XL_ERR;
     }
 
-    status = xo_orbit_get_osv_compute_validity(&orbit_id, &val_time);
-
+    // Loop over dates
     for (int i = 0; i < quant_dates; i++){
-        struct Date dt = {dates[i][0], dates[i][1], dates[i][2], dates[i][3], dates[i][4], dates[i][5], dates[i][6]};
-        double seconds = (dt.hh * 3600) + (dt.mm * 60) + dt.ss + dt.microsecs/1000000.0;
-        double start_time     = getDifference(dt);
-        vstart2 = start_time + seconds / 86400.;
-        vstop2 = vstart2 + 0.5;
-        val_time0 = vstart2;
-        val_time1 = vstop2;
-#ifdef DEBUG
-        printf("\n\t-  validity times = ( %f , %f )", val_time0, val_time1 );
-#endif
-        time = val_time0;
+        struct Date dt = {dates[i][0], dates[i][1], dates[i][2],
+                          dates[i][3], dates[i][4], dates[i][5], dates[i][6]};
 
-        status = xo_osv_compute(&orbit_id, &propag_model, &time_ref_utc, &time,
-                /* outputs */
-                                pos, vel, acc, errors);
-
-        if (status != XO_OK)
-        {
-            func_id = XO_OSV_COMPUTE_ID;
-            xo_get_msg(&func_id, errors, &n, msg);
-            xo_print_msg(&n, msg);
-        }
-#ifdef DEBUG
-        printf( "\n\t-  time = %lf (%s)", time,  to_timestamp(&time_id, time));
-        printf( "\n\t-  pos[0] = %lf", pos[0] );
-        printf( "\n\t-  pos[1] = %lf", pos[1] );
-        printf( "\n\t-  pos[2] = %lf", pos[2] );
-        printf( "\n\t-  vel[0] = %lf", vel[0] );
-        printf( "\n\t-  vel[1] = %lf", vel[1] );
-        printf( "\n\t-  vel[2] = %lf", vel[2] );
-        printf( "\n\t-  acc[0] = %lf", acc[0] );
-        printf( "\n\t-  acc[1] = %lf", acc[1] );
-        printf( "\n\t-  acc[2] = %lf", acc[2] );
-#endif
-        extra_choice = XO_ORBIT_EXTRA_NO_RESULTS;
-
-        status = xo_osv_compute_extra(&orbit_id, &extra_choice,
-                                        orbit_model_out, orbit_extra_out, errors);
-        if (status != XO_OK)
-        {
-            func_id = XO_OSV_COMPUTE_EXTRA_ID;
-            xo_get_msg(&func_id, errors, &n, msg);
-            xo_print_msg(&n, msg);
+        status = compute_position_for_date(&orbit_id, &time_id, &dt, XO_PROPAG_MODEL_TLE, pos, vel, acc, errors);
+        if (status != XL_OK) {
+            return XL_ERR;
         }
 
         position_returns[i][0] = pos[0];
@@ -457,11 +405,7 @@ __declspec(dllexport) int  get_satellite_position_tle(
         position_returns[i][2] = pos[2];
     }
 
-
-
     /* Calling to xo_orbit_close */
-    /* ------------------------- */
-
     status = xo_orbit_close(&orbit_id, errors);
     if (status != XO_OK)
     {
@@ -476,11 +420,8 @@ __declspec(dllexport) int  get_satellite_position_tle(
 
 
 #define STR_SIZE_INPUT 1000
-#define TYPE_INPUT_OSF 0
-#define TYPE_INPUT_TLE 1
 
 int main (int argc, char *argv[]){
-    // Main code can be called to perform tle calculations, as the shared library doesn't seem to work.
     int n_dates, file_input_type;
     long sat_id;
     long norad;
@@ -488,8 +429,14 @@ int main (int argc, char *argv[]){
     char *orbit_file = (char*)malloc(sizeof(char) * STR_SIZE_INPUT);
     char *sat_name = (char*)malloc(sizeof(char) * STR_SIZE_INPUT);
     char *intdes = (char*)malloc(sizeof(char) * STR_SIZE_INPUT);
+    long status = XL_OK;
+    if (tle_file == NULL || orbit_file == NULL || sat_name == NULL || intdes == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
     if(argc < 6){
-        file_input_type = TYPE_INPUT_TLE;
+        file_input_type = INPUT_TYPE_TLE;
         n_dates = 1;
     }else{
         file_input_type = atoi(argv[1]);
@@ -497,10 +444,24 @@ int main (int argc, char *argv[]){
     }
     int **dates = (int**)malloc(sizeof(int*)*n_dates);
     double **positions = (double**)malloc(sizeof(double*)*n_dates);
+    if (dates == NULL || positions == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        free(tle_file); free(orbit_file); free(sat_name); free(intdes);
+        return 1;
+    }
     for(int i = 0; i < n_dates; i++){
         positions[i] = (double*)malloc(sizeof(double)*3);
         dates[i] = (int*)malloc(sizeof(int)*7);
+        if (positions[i] == NULL || dates[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            // cleanup
+            for (int j=0; j<i; j++) { free(positions[j]); free(dates[j]); }
+            free(positions); free(dates);
+            free(tle_file); free(orbit_file); free(sat_name); free(intdes);
+            return 1;
+        }
     }
+
     if(argc < 6){
         sat_id = 200;
         dates[0][0] = 2022; dates[0][1] = 1; dates[0][2] = 1; dates[0][3] = 0; dates[0][4] = 12; dates[0][5] = 0; dates[0][6] = 0;
@@ -509,7 +470,7 @@ int main (int argc, char *argv[]){
         norad = 39159;
         strcpy(sat_name, "PROBA-V");
         strcpy(intdes, "13021A");
-    }else if(file_input_type == TYPE_INPUT_TLE){
+    }else if(file_input_type == INPUT_TYPE_TLE){
         sat_id = atol(argv[3]);
         norad = atol(argv[4]);
         strcpy(tle_file, argv[5]);
@@ -519,7 +480,7 @@ int main (int argc, char *argv[]){
         for(int i = 0; i < n_dates; i++){
             sscanf(argv[9+i], "%d-%d-%dT%d:%d:%d.%d", &dates[i][0], &dates[i][1], &dates[i][2], &dates[i][3], &dates[i][4], &dates[i][5], &dates[i][6]);
         }
-        get_satellite_position_tle(
+        status = get_satellite_position_tle(
             sat_id,
             n_dates,
             dates,
@@ -530,13 +491,13 @@ int main (int argc, char *argv[]){
             tle_file,
             positions
         );
-    }else if (file_input_type == TYPE_INPUT_OSF){
+    }else if (file_input_type == INPUT_TYPE_OSF){
         sat_id = atol(argv[3]);
         strcpy(orbit_file, argv[4]);
         for(int i = 0; i < n_dates; i++){
             sscanf(argv[5+i], "%d-%d-%dT%d:%d:%d.%d", &dates[i][0], &dates[i][1], &dates[i][2], &dates[i][3], &dates[i][4], &dates[i][5], &dates[i][6]);
         }
-        get_satellite_position_osf(
+        status = get_satellite_position_osf(
             sat_id,
             n_dates,
             dates,
@@ -546,6 +507,8 @@ int main (int argc, char *argv[]){
     }else{
         fprintf(stderr, "Input type not understood.");
     }
+
+    // Free resources
     free(tle_file);
     free(orbit_file);
     free(sat_name);
@@ -561,5 +524,9 @@ int main (int argc, char *argv[]){
     }
     free(dates);
     free(positions);
+    if (status != XL_OK) {
+        fprintf(stderr, "function failed with code %ld\n", status);
+        return status;
+    }
     return 0;
 }
